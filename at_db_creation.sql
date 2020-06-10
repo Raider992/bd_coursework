97666fe0fef7616366c54374899457e7d4690176
Raider992 <19stargazer92@gmail.com>
2020-06-10 16:51:51 +0300

    2nd commit

--- at_db_creation.sql
+++ at_db_creation.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS my_at;
 CREATE DATABASE my_at;
+USE my_at;
+
+DROP DATABASE IF EXISTS users;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(100) COMMENT 'Фамилия',
+    email VARCHAR(120) UNIQUE,
+    phone BIGINT,
+
+    INDEX users_firstname_lastname_idx(firstname, lastname)
+);
